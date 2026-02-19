be4a70da13e08745680804364616b4506136355e
MigzReyes <johnmigzreyes0@gmail.com>
2026-02-19 19:08:39 +0800

    chore: Added .gitkeep inside folders that does not contain any files

---
+++ Areas/Member/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+//using FinSys.Models; UNCOMMENT THIS IF YOU HAVE A MODEL CLASS IN MODEL FOLEDR
+
+namespace FinSys.Areas.Member.Controllers;
+
+[Area("Member")]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    public IActionResult FinancialTransaction()
+    {
+        return View();
+    }
+
+    public IActionResult Employees()
+    {
+        return View();
+    }
+
+    public IActionResult Reports()
+    {
+        return View();
+    }
+
+    public IActionResult Settings()
+    {
+        return View();
+    }
+}
